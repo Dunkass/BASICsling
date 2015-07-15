0992aaa0f2cdf0658373fcd776af9b538fb1e771
Thomas Valentin Klink <thomasvalentinklink@gmail.com>
2015-07-15 19:37:55 +0200

    Wipe

--- Assets/Scripts/StartScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class StartScript : MonoBehaviour {
-
-	void OnMouseDown(){
-		Application.LoadLevel("Game");
-	}
-}
--- Assets/Scripts/YouWinScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class YouWinScript : MonoBehaviour {
-
-	void OnMouseDown(){
-		Application.LoadLevel("Game");
-	}
-}
